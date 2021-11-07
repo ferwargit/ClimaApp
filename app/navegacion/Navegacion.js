82597e748dbe31b93b6a4ce4f0999f7d25e50ab0
Fernando Warno <ferwar@hotmail.com>
2021-11-06 23:40:21 -0300

    React-Native-Elements para Iconos

--- app/navegacion/Navegacion.js
+++ app/navegacion/Navegacion.js
@@ -2,6 +2,8 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+import { Icon } from "react-native-elements/dist/icons/Icon";
+
 
 import HomeStack from "./HomeStack";
 import BuscarStack from "./BuscarStack";
@@ -18,12 +20,16 @@ export default function Navegacion() {
 
     return (
         <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen
-                    name="home"
-                    component={HomeStack}
-                    options={{ title: "Home" }}
-                />
+            <Tab.Navigator
+                initialRouteName="buscar"
+                tabBarOptions={{
+                    inactiveTintColor: "#646464",
+                    activeTintColor: "#00a680",
+                }}
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ color }) => screenOptions(route, color),
+                })}
+            >
                 <Tab.Screen
                     name="buscar"
                     component={BuscarStack}
@@ -34,7 +40,35 @@ export default function Navegacion() {
                     component={FavoritosStack}
                     options={{ title: "Favoritos" }}
                 />
+                <Tab.Screen
+                    name="home"
+                    component={HomeStack}
+                    options={{ title: "Home" }}
+                />
             </Tab.Navigator>
         </NavigationContainer>
     )
+}
+
+function screenOptions(route, color) {
+    
+    let iconName;
+
+    switch (route.name) {
+        
+        case "buscar":
+            iconName = "magnify";
+            break;
+        case "favoritos":
+            iconName = "star-outline";
+            break;
+        case "home":
+            iconName = "home-outline";
+            break;
+        default:
+            break;
+    }
+    return (
+        <Icon type="material-community" name={iconName} size={30} color={color} />
+    );
 }
