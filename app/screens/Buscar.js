0d1a9530ec0f21439729c19f56c8de43504f0b39
Fernando Warno <ferwar@hotmail.com>
2021-11-11 18:22:01 -0300

    Ajuste nombre de la funcion

--- app/screens/Buscar.js
+++ app/screens/Buscar.js
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 
 import Formulario from "../componentes/Formulario";
 
-export default function Home() {
+export default function Buscar() {
     return (
         <View>
             <Formulario />
