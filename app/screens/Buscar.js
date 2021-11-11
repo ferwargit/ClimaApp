ec869598f74ebbc13b1713d987f59c5e5ecc3c09
Fernando Warno <ferwar@hotmail.com>
2021-11-11 17:42:08 -0300

    importo Formulario

--- app/screens/Buscar.js
+++ app/screens/Buscar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+import Formulario from "../componentes/Formulario";
+
 export default function Home() {
     return (
         <View>
-            <Text>Buscar...</Text>
+            <Formulario />
         </View>
     )
 }
