7fb5cec4257e198d95008e9c42fdec606023a030
Fernando Warno <ferwar@hotmail.com>
2021-11-11 17:42:43 -0300

    Inicio Estructura Formulario

---
+++ app/componentes/Formulario.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Formulario = () => {
+    return (
+        <Text>Desde el Formulario</Text>
+    )
+}
+
+export default Formulario;
