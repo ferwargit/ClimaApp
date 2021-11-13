9ca8473e54ca128e0b65c4638bde495e99d9865e
Fernando Warno <ferwar@hotmail.com>
2021-11-13 00:02:37 -0300

    Se fusiona App en Buscar

--- app/screens/Buscar.js
+++ app/screens/Buscar.js
@@ -1,10 +1,43 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from 'react';
+import { View, StyleSheet, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { getWeather } from './store/actions/weatherActions';
+import Form from './components/Form';
+import Weather from './components/Weather';
+
 
 export default function Buscar() {
+
+    const [search, setSearch] = useState('');
+    const [loading, setLoading] = useState(false);
+    const dispatch = useDispatch();
+    const { data, error } = useSelector(state => state.weather);
+
+    const searchSubmitHandler = () => {
+        if (search === '') {
+            return Alert.alert('Validation', 'City name is required!', [{ text: 'OK' }]);
+        }
+
+        setLoading(true);
+        dispatch(getWeather(search, () => setLoading(false), () => setLoading(false)));
+        setSearch('');
+        Keyboard.dismiss();
+    };
+
     return (
-        <View>
-            <Text>Buscar...</Text>
-        </View>
-    )
-}
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <View style={styles.container}>
+                <Form search={search} onSetSearch={setSearch} onSubmit={searchSubmitHandler} />
+                <Weather loading={loading} data={data} error={error} />
+            </View>
+        </TouchableWithoutFeedback>
+    );
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
