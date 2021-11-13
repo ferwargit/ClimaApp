15652eeda161681c6af281f7d57f7d248c7af587
Fernando Warno <ferwar@hotmail.com>
2021-11-13 01:48:51 -0300

    Ajuste Provider store en App.js

--- App.js
+++ App.js
@@ -2,16 +2,23 @@ import React from 'react';
 import { LogBox, StatusBar } from "react-native";
 import Navegacion from './app/navegacion/Navegacion';
 
+import { Provider } from 'react-redux';
+import store from './app/store/Buscar/index';
+
 LogBox.ignoreLogs(["It appears that"]);
 
 
 export default function App() {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" backgroundColor="#00aaff"  />
-      <Navegacion />
-    </>
-  );
+    return (
+        <>
+            <Provider store={store}>
+
+                <StatusBar barStyle="dark-content" backgroundColor="#00aaff" />
+                <Navegacion />
+
+            </Provider>
+        </>
+    );
 }
 
 
